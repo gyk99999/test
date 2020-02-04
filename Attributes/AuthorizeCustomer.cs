d85244024bacfc49c7a8e7a79ed709048857cd7d
RMIT-s3458598-Liming-GUAN <42239109+RMIT-s3458598-Liming-GUAN@users.noreply.github.com>
2020-02-04 18:12:59 +1100

    Update AuthorizeCustomer.cs

--- Attributes/AuthorizeCustomer.cs
+++ Attributes/AuthorizeCustomer.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using NWBA.Models;
 
+
 namespace NWBA.Attributes
 {
     public class AuthorizeCustomerAttribute : Attribute, IAuthorizationFilter
